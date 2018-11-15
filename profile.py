3ce0313c02232954f1862529ac1a1a4dfd53e692
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-15 16:45:26 -0500

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,21 @@
+import geni.portal as portal
+import geni.rspec.pg as rspec
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+# Create a XenVM
+node = request.XenVM("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node.routable_control_ip = "true"
+
+# Run the bash file to setup Anaconda
+#node.addService(rspec.Execute(shell="sh", command="sudo chmod 755 /local/repository/setup.sh"))
+node.addService(rspec.Execute("sh", "sudo /local/repository/setup.sh"))
+rspec.addResource(node)
+
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command='git clone https://github.com/longld/peda.git ~/peda'))
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command='echo "source ~/peda/peda.py" >> ~/.gdbinit'))
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec()
