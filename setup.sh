ae29a8418523b585ec7278d0d74e7ac183ab272e
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-15 16:45:51 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+
+#
+# update Ubuntu's repository
+#
+sudo apt-get -y update
+
+#
+# setup nmap
+#
+sudo apt-get -y install nmap
+
+
+#
+# setup Anaconda
+#
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
+sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
+
+# create a user named seed with password dees. 
+sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
+
+# add seed to sudo
+sudo usermod -a -G sudo seed
